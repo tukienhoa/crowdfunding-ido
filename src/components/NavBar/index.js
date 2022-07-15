4f6b12f1f69b37ce9b868ade15eefdcf3c309362
tukienhoa <tukienhoa1710@gmail.com>
2022-07-15 16:24:07 +0700

    Add login + logout metamask account

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,26 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import { Nav, Navbar, Container, Button, Form, Dropdown, DropdownButton, InputGroup } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Nav, Navbar, Container, Button, Dropdown, DropdownButton } from 'react-bootstrap';
+import {ethers} from 'ethers';
 
 import {getCookie} from '../../utils/cookie';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../NavBar/NavBar.css';
 
-const NavBar = ({isLoggedIn}) => {
+const NavBar = () => {
+
+    const [account, setAccount] = useState(getCookie("account"));
+    const [balance, setBalance] = useState(getCookie("balance"));
+
+    const [loginStatus, setLoginStatus] = useState(getCookie("account") ? true : false);
 
     const handleLogOut = () => {
-        document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        document.cookie = "id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        document.cookie = "privateKey=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        document.cookie = "publicKey=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        document.cookie = "account=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
         document.cookie = "balance=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 
         window.location.href = "/";
     }
 
-    const connectWallet = () => {
-        alert('asd');
+    const updateAccountInfo = (address) => {
+        window.ethereum.request({ 
+            method: "eth_getBalance", 
+            params: [address, "latest"] 
+        })
+        .then((balance) => {
+            setAccount(address);
+            setBalance(ethers.utils.formatEther(balance));
+
+            document.cookie = `account=${address}; max-age=86400; path=/;`;
+            document.cookie = `balance=${ethers.utils.formatEther(balance)}; max-age=86400; path=/;`;
+        });
     }
+
+    const connectWallet = () => {
+        if (window.ethereum) {
+            // Get account
+            window.ethereum.request({method:'eth_requestAccounts'})
+            .then(res => {
+                updateAccountInfo(res[0]);
+                setLoginStatus(true);
+            })
+        }
+        else {
+            alert("Please install metamask extension");
+        }
+    }
+
+    // Handle account changed
+    window.ethereum.on('accountsChanged', (accounts) => {
+        if (loginStatus) {
+            updateAccountInfo(accounts[0]);
+        }
+    });
 
     // Nav bar for not logged in user
     const MyNav = (
@@ -32,11 +66,13 @@ const NavBar = ({isLoggedIn}) => {
     // Nav bar for logged in user
     const LoggedInNav = (
         <div className="mystarter-nav">
-            <Button variant="warning" className="nav-balance-btn">Balance: {getCookie("balance")}</Button>
-            <DropdownButton variant="success" align="end" title={getCookie("publicKey").substring(0, 20) + '...'} id="dropdown-menu-align-end">
+            <Button variant="warning" className="nav-balance-btn">{balance} ETH</Button>
+            <DropdownButton variant="success" align="end" 
+                            title={account.substring(0, 5) + '...' + account.substring(account.length - 4, account.length)} 
+                            id="dropdown-menu-align-end">
                 {/* <Dropdown.Item onClick={() => handleViewWallet()}>My Wallet</Dropdown.Item> */}
                 {/* <Dropdown.Divider /> */}
-                <Dropdown.Item>Log Out</Dropdown.Item>
+                <Dropdown.Item onClick={handleLogOut}>Log Out</Dropdown.Item>
             </DropdownButton>
         </div>
     )
@@ -52,7 +88,7 @@ const NavBar = ({isLoggedIn}) => {
                     style={{ maxHeight: '100px' }}
                     navbarScroll
                 />
-                {isLoggedIn === false ? MyNav : LoggedInNav}
+                {getCookie("account") ? LoggedInNav : MyNav}
             </Navbar.Collapse>
         </Container>
         </Navbar>
