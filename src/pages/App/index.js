66e030c755790cefc4fa5580dba97cb87126e048
Tu Kien Vinh <vinh.tukien@gmail.com>
2022-08-03 15:26:22 +0700

    Add NotFound page

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -7,6 +7,7 @@ import Home from '../Home';
 import CreateProject from '../CreateProject';
 import ProjectDetails from '../ProjectDetails';
 import MyProjects from '../MyProjects';
+import NotFound from '../NotFound';
 
 const App = () => {
     return (
@@ -17,6 +18,7 @@ const App = () => {
             <Route exact path="/create-project" element={getCookie("account") ? <CreateProject /> : <Navigate replace to="/" />} />
             <Route exact path="/projects/:projectId" element={<ProjectDetails />}/>
             <Route exact path="/my-projects" element={getCookie("account") ? <MyProjects /> : <Navigate replace to="/" />}/>
+            <Route path="*" element={<NotFound />}/>
           </Routes>
       </Router>
     )
